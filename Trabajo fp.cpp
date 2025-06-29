af0ecbb956cbae9ff7d4ea56f81b77411f7694c8
Yaan768 <yaan7682019@gmail.com>
2025-06-29 18:44:54 -0500

    Agregue Mostrar contactos

--- Trabajo fp.cpp
+++ Trabajo fp.cpp
@@ -1,90 +1,98 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-struct contactoEmail {
+const int MAX_CONTACTOS = 100;
+
+struct ContactoEmail {
     string nombreCompleto;
-    char sexo;
+    string sexo;
     int edad;
     string telefono;
     string email;
     string nacionalidad;
 };
 
-const int MAX = 100;
-contactoEmail contactos[MAX];
+ContactoEmail contactos[MAX_CONTACTOS];
 int totalContactos = 0;
 
 void agregarContacto() {
-    if (totalContactos < MAX) {
-        cout << "\n--- Agregar nuevo contacto ---\n";
-        cout << "Nombre completo (sin espacios): ";
-        cin >> contactos[totalContactos].nombreCompleto;
-
-        cout << "Sexo (M/F): ";
-        cin >> contactos[totalContactos].sexo;
-
-        cout << "Edad: ";
-        cin >> contactos[totalContactos].edad;
-
-        cout << "Telefono: ";
-        cin >> contactos[totalContactos].telefono;
-
-        cout << "Email: ";
-        cin >> contactos[totalContactos].email;
-
-        cout << "Nacionalidad: ";
-        cin >> contactos[totalContactos].nacionalidad;
-
-        totalContactos++;
-        cout << "Contacto agregado correctamente.\n";
-    } else {
-        cout << "Se alcanzo el maximo de contactos.\n";
+    if (totalContactos >= MAX_CONTACTOS) {
+        cout << "No se pueden agregar más contactos.\n";
+        return;
     }
+
+    cout << "\n--- Agregar nuevo contacto ---\n";
+    cout << "Nombre completo: ";
+    cin >> contactos[totalContactos].nombreCompleto;
+    cout << "Sexo: ";
+    cin >> contactos[totalContactos].sexo;
+    cout << "Edad: ";
+    cin >> contactos[totalContactos].edad;
+    cout << "Telefono: ";
+    cin >> contactos[totalContactos].telefono;
+    cout << "Email: ";
+    cin >> contactos[totalContactos].email;
+    cout << "Nacionalidad: ";
+    cin >> contactos[totalContactos].nacionalidad;
+
+    totalContactos++;
+    cout << "Contacto agregado correctamente.\n";
 }
 
 void eliminarContacto() {
     if (totalContactos == 0) {
-        cout << "No hay contactos registrados.\n";
+        cout << "\nNo hay contactos para eliminar.\n";
         return;
     }
 
+    int posicion;
     cout << "\n--- Eliminar contacto ---\n";
-    for (int i = 0; i < totalContactos; i++) {
-        cout << i + 1 << ") " << contactos[i].nombreCompleto << " - " << contactos[i].email << endl;
-    }
+    cout << "Ingrese la posicion del contacto (1 a " << totalContactos << "): ";
+    cin >> posicion;
 
-    int pos;
-    cout << "Ingrese el numero del contacto que desea eliminar: ";
-    cin >> pos;
-
-    if (pos < 1 || pos > totalContactos) {
+    if (posicion < 1 || posicion > totalContactos) {
         cout << "Posicion invalida.\n";
         return;
     }
 
-    // Desplazar contactos
-    for (int i = pos - 1; i < totalContactos - 1; i++) {
+    for (int i = posicion - 1; i < totalContactos - 1; i++) {
         contactos[i] = contactos[i + 1];
     }
 
     totalContactos--;
-    cout << "Contacto eliminado correctamente.\n";
+    cout << "Contacto eliminado exitosamente.\n";
 }
 
-void mostrarMenu() {
-    cout << "\n===== MENU DE CONTACTOS =====\n";
-    cout << "a) Agregar un contacto\n";
-    cout << "b) Eliminar un contacto\n";
-    cout << "c) Mostrar listado general\n";
-    cout << "d) Mostrar ordenado por servidor de correo\n";
-    cout << "e) Salir\n";
-    cout << "Seleccione una opcion: ";
+void mostrarContactos() {
+    if (totalContactos == 0) {
+        cout << "\nNo hay contactos registrados.\n";
+        return;
+    }
+
+    cout << "\n--- Listado de contactos ---\n";
+    for (int i = 0; i < totalContactos; i++) {
+        cout << "\nContacto #" << i + 1 << ":\n";
+        cout << "Nombre: " << contactos[i].nombreCompleto << endl;
+        cout << "Sexo: " << contactos[i].sexo << endl;
+        cout << "Edad: " << contactos[i].edad << endl;
+        cout << "Telefono: " << contactos[i].telefono << endl;
+        cout << "Email: " << contactos[i].email << endl;
+        cout << "Nacionalidad: " << contactos[i].nacionalidad << endl;
+    }
 }
 
 int main() {
     char opcion;
+
     do {
-        mostrarMenu();
+        cout << "\n===== MENU DE GESTION DE CONTACTOS =====\n";
+        cout << "a) Agregar un contacto\n";
+        cout << "b) Eliminar un contacto\n";
+        cout << "c) Mostrar listado general de contactos\n";
+        cout << "d) Mostrar listado ordenado por servidor de correo (en desarrollo)\n";
+        cout << "e) Salir del programa\n";
+        cout << "Seleccione una opcion: ";
         cin >> opcion;
 
         switch (opcion) {
@@ -95,21 +103,23 @@ int main() {
                 eliminarContacto();
                 break;
             case 'c':
-                cout << "Mostrar listado general ";
+                mostrarContactos();
                 break;
             case 'd':
-                cout << "[Mostrar ordenado por servidor ";
+                cout << "Funcion en desarrollo...\n";
                 break;
             case 'e':
                 cout << "Saliendo del programa...\n";
                 break;
             default:
-                cout << "Opcion invalida. Intente de nuevo.\n";
+                cout << "Opcion invalida. Intente nuevamente.\n";
         }
+
     } while (opcion != 'e');
 
     return 0;
 }
+
 
 
 
